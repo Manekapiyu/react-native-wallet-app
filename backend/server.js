bb9cd4d215c22d50f918b66c24f2d8b4c384f7f2
Manekapiyu <162763930+Manekapiyu@users.noreply.github.com>
2025-12-10 21:48:55 +0530

    Database Setup

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,40 @@
 import express from "express";
+import dotenv from "dotenv";
+import { sql } from "./config/db.js";
 
+dotenv.config();
 
 const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+async function initDB() {
+    try{
+        await sql`CREATE TABLE IF NOT EXISTS transactions(
+        id SERIAL PRIMARY KEY,
+        user_id VARCHAR(255) NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        amount DECIMAL(10,2) NOT NULL,
+        category VARCHAR(100),
+        create_at DATE NOT NULL DEFAULT CURRENT_DATE
+    )`;
+
+    console.log("Database initializing Successful");
+
+    }catch (error){
+        console.error("Error initializing database:", error);
+        process.exit(1);
+
+    }
+}
 
 app.get("/",(req,res)=>{
     res.send("It is working");
 });
 
-app.listen(5001,()=>{
-    console.log("Server is running on port 5001");
+
+initDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Server is running on port ${PORT}`);
+    });
 });

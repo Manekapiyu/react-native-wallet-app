3f1fdc018ad832d8d9b96fc74bebc2525a32b3b2
Manekapiyu <162763930+Manekapiyu@users.noreply.github.com>
2025-12-14 01:09:43 +0530

    development compeleted

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,49 +1,37 @@
 import express from "express";
 import dotenv from "dotenv";
-import { sql } from "./config/db.js";
+import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
-import transactionsRoute from "./routes/transactionsRoute.js"
-
+import transactionsRoute from "./routes/transactionsRoute.js";
+import job from "./config/cron.js";
 
 dotenv.config();
 
 const app = express();
 
-// Middleware to parse JSON body
+if (process.env.NODE_ENV === "production") job.start();
+
+// middleware
 app.use(rateLimiter);
 app.use(express.json());
 
+// our custom simple middleware
+// app.use((req, res, next) => {
+//   console.log("Hey we hit a req, the method is", req.method);
+//   next();
+// });
+
 const PORT = process.env.PORT || 5001;
 
-// Initialize DB
-async function initDB() {
-  try {
-    await sql`
-      CREATE TABLE IF NOT EXISTS transactions(
-        id SERIAL PRIMARY KEY,
-        user_id VARCHAR(255) NOT NULL,
-        title VARCHAR(255) NOT NULL,
-        amount DECIMAL(10,2) NOT NULL,
-        category VARCHAR(100),
-        create_at DATE NOT NULL DEFAULT CURRENT_DATE
-      )
-    `;
-
-    console.log("Database initialized successfully");
-  } catch (error) {
-    console.error("Error initializing database:", error);
-    process.exit(1);
-  }
-}
-
-
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
 
 app.use("/api/transactions", transactionsRoute);
 
-// Start server after DB init
 initDB().then(() => {
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log("Server is up and running on PORT:", PORT);
   });
 });
